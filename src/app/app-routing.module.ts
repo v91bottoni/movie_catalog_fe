0b1403a781ab0e17394d5ffcc838dd8b26660593
davidecafagno <davidecafagno@hotmail.it>
2023-07-05 17:59:29 +0200

    Commit Aggiunta Modifica FIlm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateMovieComponent } from './components/update-movie/update-movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'updateMovie',
+    component: UpdateMovieComponent,
+    /*canActivate: [],
+    data:{
+        expectedRoles: ['super_admin', 'admin']
+    },*/
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { UpdateMovieComponent } from './components/update-movie/update-movie.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule ,ReactiveFormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateMovieComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
