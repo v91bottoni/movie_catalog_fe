c00749b02bcbd4ffe582803715e9d4ce60b54390
davidecafagno <davidecafagno@hotmail.it>
2023-07-11 15:01:40 +0200

    Utility Update

--- src/app/service/role-guard.service.ts
+++ src/app/service/role-guard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { UtilityService } from './utility.service';
 
 
 @Injectable({
@@ -7,7 +8,7 @@ import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 })
 export class RoleGuardService implements CanActivate{
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private util:UtilityService) { }
 
   canActivate(route: ActivatedRouteSnapshot):boolean {
     const expectedRoles: string[] = route.data['expectedRoles'];
@@ -15,6 +16,8 @@ export class RoleGuardService implements CanActivate{
     let token: boolean = false;
 
     if(role == '') {
+      this.util.username = null;
+      this.util.role = null;
       this.router.navigateByUrl('/login');
       return false;
     }
@@ -23,8 +26,10 @@ export class RoleGuardService implements CanActivate{
       if(element == role)   token = true;
     });
 
-    
+
     if(!token){
+      this.util.username = null;
+      this.util.role = null;
       this.router.navigateByUrl('/login');
       return false;
     }
