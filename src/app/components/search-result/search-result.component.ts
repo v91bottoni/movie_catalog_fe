0b5760bcc8d7ae5db2776143db1353f9c0a32fa0
AntonioLoSapio <antonio.losapio93s@gmail.com>
2023-07-12 11:26:02 +0200

    Ricerca Movie - Dialog Risolta Moficia icon Person User Risolti errori in MovieDetails

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -3,7 +3,9 @@ import { Router } from '@angular/router';
 import { Movie } from 'src/app/models/movie';
 import { MovieService } from 'src/app/service/movie.service';
 import { SearchService } from 'src/app/service/search.service';
-import { UtilityService } from 'src/app/service/utility.service';
+import  { UtilityService } from 'src/app/service/utility.service';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 
 @Component({
   selector: 'app-search-result',
@@ -12,14 +14,15 @@ import { UtilityService } from 'src/app/service/utility.service';
 })
 export class SearchResultComponent {
   movieList!:Movie[];
+  movie!: Movie;
   title :string|undefined = undefined;
   maxPage = 0;
   page = 1;
   displayedColumns: string[] = ['title', 'plot', 'writer' ,'imdbrating'];
   cardView:boolean = true;
   constructor(private service:MovieService, private router: Router,
-    private searchService:SearchService, private util:UtilityService) {
-      this.util.backpage = "search";
+    private searchService:SearchService, public dialog: MatDialog,public utilityService:UtilityService) {
+      this.utilityService.backpage = "search";
       setTimeout(()=>{
         if(this.title == undefined){
           // this.title = this.searchService.oldSearch;
@@ -61,4 +64,13 @@ export class SearchResultComponent {
     this.router.navigate(['/movies', movieId]);
   }
 
+
+  openDialog(imdbid: string){
+    this.service.movieid = imdbid;
+    const dialogRef = this.dialog.open(MovieDetailsComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
