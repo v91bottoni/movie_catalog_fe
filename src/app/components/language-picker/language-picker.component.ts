7001484b9af509d35aac67dfee8d2b422d1db9b1
davidecafagno <davidecafagno@hotmail.it>
2023-07-18 17:35:12 +0200

    Merge Conflict

--- src/app/components/language-picker/language-picker.component.ts
+++ src/app/components/language-picker/language-picker.component.ts
@@ -9,12 +9,12 @@ import { TranslateService } from '@ngx-translate/core';
 export class LanguagePickerComponent implements OnInit{
 
   readonly languages = [
-    { value: 'en', label: 'English', img: 'assets/icons/gb.svg' }, 
+    { value: 'en', label: 'English', img: 'assets/icons/gb.svg' },
     { value: 'it', label: 'Italiano', img: 'assets/icons/it.svg'}
   ];
 
   selectedLanguage = this.languages[0];
-  
+
   ngOnInit(): void {
     if(localStorage.getItem('lang')){
       let lang = localStorage.getItem('lang');
@@ -29,7 +29,7 @@ export class LanguagePickerComponent implements OnInit{
 
   changeLang(lang: string){
     console.log(lang);
-    
+
     this.selectedLanguage = this.languages.find( language => language.value === lang) || this.languages[0];
     localStorage.setItem('lang', this.selectedLanguage.value);
     this.translate.use(lang);
